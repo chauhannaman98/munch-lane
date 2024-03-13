472e315b68d5963d23b58581b547aecb6dc7c323
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-13 19:31:39 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header 
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCards
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
