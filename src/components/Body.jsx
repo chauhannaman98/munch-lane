c38b871123b9c5b7057e0250a764412447e85782
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-15 22:48:48 +0530

    React Hooks and Virtual Dom

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,29 @@
 import RestaurantCards from "./RestaurantCards";
 import resList from '../utils/mockData';
+import { useState } from "react";
 
 const Body = () => {
+    // state variable
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
     return (
         <div className="body">
-            <div className="search">Search Bar</div>
+            <div className="filter">
+                <button
+                    className="btn-filter"
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredList);
+                    }}
+                >
+                    Top Rated Restaurant
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map(restaurant => (
+                    listOfRestaurants.map(restaurant => (
                         <RestaurantCards key={restaurant.info.id} ResData={restaurant} />
                     ))
                 }
