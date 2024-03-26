0a7a4bb659a4141416c794c713e64c4ac1b50d8c
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-26 18:23:10 +0530

    unit testing 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Us!</h1>
-            <h2>Email us at techmirtz@gmail.com</h2>
+            <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+            <form className="m-6">
+                <input
+                    type="text"
+                    className=" border border-black p-2 m-2"
+                    placeholder="name"
+                />
+                <input
+                    type="text"
+                    className=" border border-black p-2 m-2"
+                    placeholder="message"
+                />
+                <button className=" border border-black p-2 m-2 bg-gray-100 rounded-lg">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
-
 export default Contact;
