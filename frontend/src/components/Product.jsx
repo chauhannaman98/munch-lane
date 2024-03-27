6ad5564d58ff9f90ac2c67633c55dcb79309729c
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-27 22:22:33 +0530

    Added products

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,25 @@
+import { Card } from "react-bootstrap"
+
+const Product = ({ product }) => {
+    return (
+        <Card className="my-3 p-3 rounded">
+            <a href={`/product/${product.id}`}>
+                <Card.Img src={product.image} variant="top" />
+            </a>
+
+            <Card.Body>
+                <a href={`/product/${product.id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as="h3">
+                    ₹ {product.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
