fb83b66cd24de4d871ec74bb27391a3e37589094
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-27 23:18:20 +0530

    Added product screen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,27 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <Link to={`/product/${product.id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant="top" />
             </Link>
 
             <Card.Body>
-                <Link to={`/product/${product.id}`}>
-                    <Card.Title as="div">
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as="div" className="product-title">
                         <strong>{product.name}</strong>
                     </Card.Title>
                 </Link>
+
+                <Card.Text as='div'>
+                    <Rating
+                        value={product.rating}
+                        text={`${product.numReviews} reviews`}
+                    />
+                </Card.Text>
 
                 <Card.Text as="h3">
                     ₹ {product.price}
