f484d7fe7a981a34bcbd3d26b7948d3eae0345f6
Naman Chauhan <chauhannaman98@gmail.com>
2024-03-29 23:15:37 +0530

    added cart badge

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+    console.log(cartItems);
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -19,7 +23,16 @@ const Header = () => {
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
                             <LinkContainer to='/cart'>
-                                <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                    {
+                                        cartItems.length > 0 && (
+                                            <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                                {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                            </Badge>
+                                        )
+                                    }
+                                </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to='/login'>
                                 <Nav.Link><FaUser /> Sign In</Nav.Link>
