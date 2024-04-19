df21bcb7bd92c5af541576b3de52f7e57a240bed
Naman Chauhan <chauhannaman98@gmail.com>
2024-04-19 18:56:13 +0530

    removed unused entitites

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Form, Button } from 'react-bootstrap';
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from "../slices/cartSlice";
